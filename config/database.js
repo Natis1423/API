c86c7ef819dfb007bc0aa2f485db8c3bc9ee0bc4
unknown <irmarios0115@gmail.com>
2024-06-27 12:58:57 -0500

    Ya subi

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const db={
+    host:'roundhouse.proxy.rlwy.net',
+    port:45748,
+    user:'root',
+    password:'diUIGwpBXMKUIHsAWZHkDHJgmwXgFOLd',
+    database:'railway',
+}
+
+export default db
